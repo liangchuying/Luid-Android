bfa545d9a57ec2c40a9eb2ecc81e7c2263a25f65
liangchuying <383696165@qq.com>
2024-07-24 23:20:57 +0800

    fix:添加视频缓冲

--- videolibrary/src/main/java/com/example/videolibrary/VideoActivity.kt
+++ videolibrary/src/main/java/com/example/videolibrary/VideoActivity.kt
@@ -10,23 +10,54 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.LoadControl
 import androidx.media3.exoplayer.source.ProgressiveMediaSource
 import androidx.media3.ui.PlayerView
+
 
 class VideoActivity : AppCompatActivity() {
 
     lateinit var playerView: PlayerView
     lateinit var player: ExoPlayer
+
+    @UnstableApi
+    lateinit var loadControl: LoadControl
+
     private var toolbar: Toolbar? = null
-    @OptIn(UnstableApi::class) private fun initView() {
-        val url: String = "https://cs-example.obs.cn-south-1.myhuaweicloud.com/t45/%E7%B3%BB%E7%BB%9F%E6%96%87%E4%BB%B6/%E5%AE%A2%E6%9C%8D/video/%E4%B8%8B%E8%BD%BD.mp4?AccessKeyId=HKXFQ7HJT01TX8USG2RX&Expires=1752711840&Signature=15A4t%2BXEj/zFEJIsv8DJJHwQJZU%3D"
+
+    @OptIn(UnstableApi::class)
+    private fun initView() {
+        val url: String =
+            "https://cs-example.obs.cn-south-1.myhuaweicloud.com/t45/%E7%B3%BB%E7%BB%9F%E6%96%87%E4%BB%B6/%E5%AE%A2%E6%9C%8D/video/%E4%B8%8B%E8%BD%BD.mp4?AccessKeyId=HKXFQ7HJT01TX8USG2RX&Expires=1752711840&Signature=15A4t%2BXEj/zFEJIsv8DJJHwQJZU%3D"
 
         playerView = findViewById(R.id.video_view)
 
-        player = ExoPlayer.Builder(this).build()
+        playerView.setControllerShowTimeoutMs(3000); // 控制器显示超时时间
+
+        loadControl = DefaultLoadControl.Builder()
+            .setBufferDurationsMs(
+                15000,  // 最小缓冲时间 15秒
+                50000,  // 最大缓冲时间 50秒
+                1000,   // 播放所需最小缓冲时间 1秒
+                5000    // 重新缓冲后播放所需最小缓冲时间 5秒
+            ).build()
+
+        player = ExoPlayer.Builder(this)
+            .setLoadControl(loadControl)
+            .build()
+
+        player.addListener(object : Player.Listener {
+            override fun onPlayerError(error: PlaybackException) {
+                // 处理播放错误
+                Log.e("ExoPlayer---", "Error: " + error.message)
+            }
+        })
 
         playerView.player = player
 
@@ -41,6 +72,8 @@ class VideoActivity : AppCompatActivity() {
         player.setMediaItem(mediaItem)
         player.prepare()
         player.play()
+
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,9 +89,10 @@ class VideoActivity : AppCompatActivity() {
         }
 
         toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar)
+//        setSupportActionBar(toolbar)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
         toolbar.setNavigationOnClickListener {
             finish()
         }
@@ -66,11 +100,19 @@ class VideoActivity : AppCompatActivity() {
         initView()
     }
 
+    @OptIn(UnstableApi::class)
     override fun onDestroy() {
         super.onDestroy()
 
-        Log.e("Tag", "我退出了吗？")
-        player.release()
-    }
-}
+        player.pause()
 
+        player.release()
+        Log.e("Tag", "我退出了吗？")
+    }
+
+
+//    fun  initialMediaCode() {
+//        val mediaCodeInstance = MediaCodec.createDecoderByType(MimeTypes.VIDEO_H265)
+//
+//    }
+}
